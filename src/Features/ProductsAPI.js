4f4c4df141c25860ac195e5ed31398a0c722049b
Adarsh Valamary <adarsh.valamary@capgemini.com>
2023-02-22 19:36:59 +0530

    JSON path change

--- src/Features/ProductsAPI.js
+++ src/Features/ProductsAPI.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "../Assets/Data/game.json" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://raw.githubusercontent.com/adarshmahe/playstation/main/public/game.json" }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => ``,
